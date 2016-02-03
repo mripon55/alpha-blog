acda912134a96c2b8abd63104d0e385974e3895f
mripon <mripon@hotmail.com>
2016-02-03 19:46:53 +0000

    Added temp User Id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
-    validates :username, presence: true, length: {minimum: 3, maximum: 50}
-    validates :email, presence: true, length: {minimum: 10, maximum: 300}
+has_many :articles
+before_save { self.email = email.downcase }
+validates :username, presence: true,
+uniqueness: { case_sensitive: false },
+length: { minimum: 3, maximum: 25 }
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :email, presence: true, length: { maximum: 105 },
+uniqueness: { case_sensitive: false },
+format: { with: VALID_EMAIL_REGEX }
 end
